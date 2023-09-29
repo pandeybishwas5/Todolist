abd1598a276f7f74dce602cb0acf81a4a6222e58
Bishwas Pandey <pandeybishwas5@gmail.com>
2023-09-29 16:25:01 +1000

    Initila commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const port = 3000;
+const app = express();
+var items = [];
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+})
+
+
+app.post("/submit", (req,res) =>{
+    var newList = req.body["newItem"];
+    items.push(newList);
+    res.render("index.ejs", {items});
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
