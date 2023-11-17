875b0db693964931cd98fc79014df8a7fb9ce481
Bishwas Pandey <pandeybishwas5@gmail.com>
2023-11-17 23:56:06 +1100

    Add Styles

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ import bodyParser from "body-parser";
 const port = 3000;
 const app = express();
 const items = [];
-const completed = [];
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 // Routes
 app.get("/", (req, res) => {
-    res.render("index.ejs", {items, completed});
+    res.render("index.ejs", {listItems: items});
 })
 
 
@@ -19,12 +19,13 @@ app.post("/submit", (req,res) =>{
     var newItem = req.body["newItem"];
     if (newItem) {
         items.push(newItem);
-        completed.push(false);
     }
     res.redirect("/");
 });
 
-
+app.post("/delete", (req,res) => {
+    
+})
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
